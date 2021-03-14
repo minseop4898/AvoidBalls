b72b68521e291589c089c1f707d6e03db1bc88b4
Minseop <minseop4898@naver.com>
2021-03-14 20:10:40 +0900

    Modify agent movement with GetKey method, not player brain

--- Assets/ML-Agents/AvoidBalls/Scripts/AvoidBallsAgent.cs
+++ Assets/ML-Agents/AvoidBalls/Scripts/AvoidBallsAgent.cs
@@ -19,33 +19,23 @@ public class AvoidBallsAgent : Agent
 
     public override void AgentAction(float[] vectorAction, string textAction)
 	{
-        if (vectorAction[0] == 5)
+        Vector3 moveVector = new Vector3(0f,0f,0f);
+        float moveScale = 0.1f;
+
+        if (Input.GetKey(KeyCode.A)) {moveVector += new Vector3(-1f * moveScale,0f,0f);}
+        if (Input.GetKey(KeyCode.S)) {moveVector += new Vector3(0f,0f,-1f * moveScale);}
+        if (Input.GetKey(KeyCode.D)) {moveVector += new Vector3(1f * moveScale,0f,0f);}
+        if (Input.GetKey(KeyCode.W)) {moveVector += new Vector3(0f,0f,1f * moveScale);}
+
+        user.transform.position += moveVector;
+        
+        if (Input.GetKeyDown(KeyCode.Space) && moveVector != new Vector3(0f,0f,0f))
         {
-            Vector3 userVelo = user.GetComponent<Rigidbody>().velocity;
-            Vector3 userPos = user.transform.position;
-            GameObject newBall = (GameObject)Instantiate(ball, userPos + userVelo, Quaternion.identity);
-            newBall.GetComponent<Rigidbody>().AddForce(userVelo);
-        }
-        else
-        {
-            Vector3 direction = new Vector3(0f, 0f, 0f);
-            float velScale = 5f;
-            switch (vectorAction[0])
-            {
-                case 1:
-                    direction.x = -1f * velScale;
-                    break;
-                case 2:
-                    direction.z = -1f * velScale;
-                    break;
-                case 3:
-                    direction.x = 1f * velScale;
-                    break;
-                case 4:
-                    direction.z = 1f * velScale;
-                    break;
-            }
-            user.GetComponent<Rigidbody>().AddForce(direction);
+            GameObject newBall = (GameObject)Instantiate(
+                ball, user.transform.position + 2 * moveVector, Quaternion.identity
+            );
+            newBall.GetComponent<Rigidbody>().AddForce(moveVector * 3000f);
+            ballArray.Add(newBall);
         }
     }
 
